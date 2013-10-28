85e3763807af8f5f62a303d779ff178c0236f595
WU YONGSHOU <footballlifer@hotmail.com>
2013-10-29 01:03:04 +0900

    image test

--- FBLS/src/nanofuntas/fbls/FBLServlet.java
+++ FBLS/src/nanofuntas/fbls/FBLServlet.java
@@ -1,5 +1,8 @@
 package nanofuntas.fbls;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
@@ -7,6 +10,7 @@ import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 
+import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -46,10 +50,44 @@ public class FBLServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		if(DEBUG) System.out.println(TAG + ": doPost()");						
 		
-		// get JSON request parameters handle it and finally send it
-		JSONObject jsonReq = getJsonReq(request);		
-		JSONObject jsonRsp = DataHandler.handleData(jsonReq);		
-		sendJsonRsp(response, jsonRsp);
+		//TODO: image test
+		if (request.getHeader("Content-type").equals("image/png")) {
+			System.out.println("Content-type:image/png");
+			
+			String absoluteStuffPath = getServletContext().getRealPath("/");
+			System.out.println("kakpple path:"+absoluteStuffPath);
+			
+			String path = absoluteStuffPath + "cr" + ".png";
+			BufferedImage bImageFromConvert = ImageIO.read(request.getInputStream());
+			
+			ImageIO.write(bImageFromConvert, "png", new File(path));
+		} else if (request.getHeader("Content-type").equals("get/image")) {
+			System.out.println("mytype-type:get/image");
+			
+			String absoluteStuffPath = getServletContext().getRealPath("/");
+			System.out.println("kakpple path:"+absoluteStuffPath);
+			
+			String path = absoluteStuffPath + "cr" + ".png";
+			
+			FileInputStream in = new FileInputStream(path);
+			OutputStream outputstream = response.getOutputStream();
+
+		    // Copy the contents of the file to the output stream
+			byte[] buf = new byte[1024];
+			int count = 0;
+			while ((count = in.read(buf)) >= 0) {
+				outputstream.write(buf, 0, count);
+			}
+			outputstream.close();
+		    in.close();
+			
+		} 
+		else {
+			// get JSON request parameters handle it and finally send it
+			JSONObject jsonReq = getJsonReq(request);		
+			JSONObject jsonRsp = DataHandler.handleData(jsonReq);		
+			sendJsonRsp(response, jsonRsp);
+		}
 	}
 
 	/**
